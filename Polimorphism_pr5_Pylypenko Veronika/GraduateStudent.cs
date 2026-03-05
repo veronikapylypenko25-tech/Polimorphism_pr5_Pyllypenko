b61928485725f89293f0fe1ba63a307bc0c82a83
Veronika Pylypenko <veronikapylypenko25@kse.org.ua>
2026-03-05 21:20:14 +0200

    Adding and update of GraduateStudent class

---
+++ Polimorphism_pr5_Pylypenko Veronika/GraduateStudent.cs
@@ -0,0 +1,14 @@
+﻿namespace Polimorphism_pr5_Pylypenko_Veronika;
+
+public class GraduateStudent : UndergraduateStudent
+{
+    public   GraduateStudent(string name, string id) : base(name, id)
+    {
+        
+    }
+    public override void PerformDuties()
+    {
+        base.PerformDuties();
+        ActionLog.Add("Thesis research update");
+    }
+}
