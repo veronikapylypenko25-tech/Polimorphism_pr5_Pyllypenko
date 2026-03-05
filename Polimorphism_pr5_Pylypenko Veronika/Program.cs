94a0e580ba6371a5e1b35eccba91e4aac2a91cae
Veronika Pylypenko <veronikapylypenko25@kse.org.ua>
2026-03-05 21:43:23 +0200

    Last update of programm class

--- Polimorphism_pr5_Pylypenko Veronika/Program.cs
+++ Polimorphism_pr5_Pylypenko Veronika/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-       
+        UniversityRegistry registry = new UniversityRegistry();
+        Proffesor proffesor = new Proffesor("Dr. Meier", "10K");
+        UndergraduateStudent undergraduate = new UndergraduateStudent("Nika", "11M");
+        GraduateStudent graduate = new GraduateStudent("Kira", "12N");
+        registry.AddMember(proffesor);
+        registry.AddMember(undergraduate);
+        registry.AddMember(graduate);
+        registry.ExecuteAllDuties();
+        proffesor.ConductResearch("OOP");
+        Console.WriteLine("Total actions: "+ registry.GetMemberStatistics());
     }
 }
--- Polimorphism_pr5_Pylypenko Veronika/UniversityRegistry.cs
+++ Polimorphism_pr5_Pylypenko Veronika/UniversityRegistry.cs
@@ -24,7 +24,6 @@ public class UniversityRegistry
         {
             totalActions += member.GetActionCount();
         }
-
         return totalActions;
     }
 }
