f4ddd9f65a21b52cd06bb6c25a5daeb571bc0af9
Veronika Pylypenko <veronikapylypenko25@kse.org.ua>
2026-03-05 20:23:06 +0200

    Added UniversityMember classes

---
+++ Polimorphism_pr5_Pylypenko Veronika/UniversityMember.cs
@@ -0,0 +1,25 @@
+﻿namespace Polimorphism_pr5_Pylypenko_Veronika;
+
+public class UniversityMember
+{
+    private string _name;
+
+    public string Name
+    {
+        get=>_name;
+        set=>_name=string.IsNullOrEmpty(value)? throw new Exception("Name cannot be empty!"):value;
+    }
+    public string MemberID { get; private set; }
+    protected List<string> ActionLog = new List<string>();
+
+    public UniversityMember(string name, string id)
+    {
+        Name = name;
+        MemberID = id;
+    }
+
+    virtual void PerformDuties()
+    {
+        if (ActionLog.Count>=5){throw new Exception()}
+    }
+}
