34815dea00d1efaad2e70f1f2f5059610bdfb4a1
Veronika Pylypenko <veronikapylypenko25@kse.org.ua>
2026-03-05 21:14:14 +0200

    Update og UndergraduateStudent class

--- Polimorphism_pr5_Pylypenko Veronika/UndergraduateStudent.cs
+++ Polimorphism_pr5_Pylypenko Veronika/UndergraduateStudent.cs
@@ -1,6 +1,17 @@
 ﻿namespace Polimorphism_pr5_Pylypenko_Veronika;
 
-public class UndergraduateStudent
+public class UndergraduateStudent : UniversityMember
 {
-    
+    public  UndergraduateStudent(string name, string id) : base(name, id)
+    {
+        
+    }
+    public override void PerformDuties()
+    {
+        if (ActionLog.Count >= 5)
+        {
+            throw new Exception("Member has already reached a daily limit of 5 actions!");
+        }
+        ActionLog.Add("Lab work completed");
+    }
 }
